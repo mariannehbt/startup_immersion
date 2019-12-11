bce3eb686753563d477fbae3cdc45a6f97090a32
mehdi <mourzagh.m@live.fr>
2019-12-11 19:35:13 +0100

    [UPDATED] Add attribut latitude and longetitude and migrate - mehdi

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_152438) do
+ActiveRecord::Schema.define(version: 2019_12_11_131339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2019_12_09_152438) do
     t.bigint "startup_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["startup_id"], name: "index_events_on_startup_id"
   end
 
