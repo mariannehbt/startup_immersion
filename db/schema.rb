f934d87bc2a62cfa2b485833e8ac1401f3141f47
mariannehbt <marianne.heribert@gmail.com>
2019-12-13 21:42:53 +0100

    [CREATE] attendances admin validation - marianne

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_142255) do
+ActiveRecord::Schema.define(version: 2019_12_13_190401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2019_12_10_142255) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "reviewed", default: false
+    t.boolean "validated", default: false
     t.index ["event_id"], name: "index_attendances_on_event_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
