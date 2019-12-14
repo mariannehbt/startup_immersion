0e91c8a8d4a9dbadd9e44457b97ad01b9846e36f
mariannehbt <marianne.heribert@gmail.com>
2019-12-14 23:20:38 +0100

    [CORRECTED] db/schema.rb after pull marceline - marianne

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_12_13_190401) do
-=======
-ActiveRecord::Schema.define(version: 2019_12_11_131339) do
->>>>>>> front
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
